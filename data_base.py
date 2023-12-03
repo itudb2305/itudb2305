af6fbc14c0116f2582b22e5595a283f3eb6659c0
itu-itis21-ahmed20 <ahmed20@itu.edu.tr>
2023-12-03 15:48:42 +0300

    creation of player pages showing the tables + creation of quizz game version 1

---
+++ data_base.py
@@ -0,0 +1,143 @@
+import mysql.connector as dbapi # mysql
+from dotenv import load_dotenv
+load_dotenv()
+import os
+HOST = os.getenv("HOST")
+PORT = os.getenv("PORT")
+USER = os.getenv("USER")
+PASSWORD = os.getenv("PASSWORD")
+
+def game_get_comp():
+    connection = dbapi.connect(host = HOST, port = PORT, user = USER, password=PASSWORD, database="futbalmania")
+
+    cursor = connection.cursor()
+
+    statement = """select competition_id, competitions_name
+                    from competitions
+                    order by competitions_name"""
+
+    cursor.execute(statement)
+    game_competitions = cursor.fetchall()
+    result = [list(comp) for comp in game_competitions]
+
+    for i in range(len(result)):
+        result[i][1] = result[i][1].replace('-', ' ')
+        result[i][1] = result[i][1].title()
+
+    cursor.close()
+    connection.close()
+
+    return result
+
+def game_get_season():
+    connection = dbapi.connect(host = HOST, port = PORT, user = USER, password=PASSWORD, database="futbalmania")
+
+    cursor = connection.cursor()
+
+    statement = """SELECT distinct(season)
+                    FROM futbalmania.games
+                    order by season DESC;"""
+
+    cursor.execute(statement)
+    seasons = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    return seasons
+
+def game_get_round():
+    connection = dbapi.connect(host = HOST, port = PORT, user = USER, password=PASSWORD, database="futbalmania")
+
+    cursor = connection.cursor()
+
+    statement = """SELECT distinct(games_round)
+                    FROM futbalmania.games
+                    ORDER BY games_round;"""
+
+    cursor.execute(statement)
+    rounds = cursor.fetchall()
+    return rounds
+
+def player_t():
+    connection = dbapi.connect(host = "localhost", port = 3306, user = "root", password="12345", database="futbalmania")
+
+    cursor = connection.cursor()
+
+    statement = 'SELECT players_name,country_of_citizenship,position FROM players;'
+
+    cursor.execute(statement)
+    result_2 = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    return result_2
+
+def game_get_clubs():
+    connection = dbapi.connect(host = HOST, port = PORT, user = USER, password=PASSWORD, database="futbalmania")
+
+    cursor = connection.cursor()
+
+    statement = """SELECT distinct(A.club_id), (A.clubs_name) FROM futbalmania.clubs A
+                    WHERE A.club_id in (SELECT B.home_club_id FROM futbalmania.games B)
+	                OR A.club_id in (SELECT C.away_club_id FROM futbalmania.games C)
+                    order by A.clubs_name;"""
+
+    cursor.execute(statement)
+    seasons = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    return seasons
+
+def game_get_games():
+    connection = dbapi.connect(host = HOST, port = PORT, user = USER, password=PASSWORD, database="futbalmania")
+
+    cursor = connection.cursor()
+
+    statement = """SELECT A.game_id, B.competitions_name, A.games_round, A.home_club_name, A.home_club_goals, A.away_club_goals, A.away_club_name, A.games_date
+                    FROM futbalmania.games A
+                    JOIN futbalmania.competitions B on A.competition_id = B.competition_id
+                    order by games_date DESC
+                    LIMIT 20 OFFSET 0;"""
+
+    cursor.execute(statement)
+    games = cursor.fetchall()
+    result = [list(comp) for comp in games]
+
+    for i in range(len(result)):
+        result[i][1] = result[i][1].replace('-', ' ')
+        result[i][1] = result[i][1].title()
+    
+    return result
+
+def question_game():
+    connection = dbapi.connect(host = "localhost", port = 3306, user = "root", password="12345", database="futbalmania")
+    
+    cursor = connection.cursor(dictionary=True)
+
+    statement = 'SELECT players_name,country_of_citizenship FROM players WHERE highest_market_value_in_eur >= 50000000 AND last_season > 2015 ORDER BY RAND() LIMIT 1;'
+   
+    cursor.execute(statement)
+    result_3 = cursor.fetchall()
+    cursor.close()
+    connection.close()
+
+    return result_3
+
+def random_value():
+    connection = dbapi.connect(host = "localhost", port = 3306, user = "root", password="12345", database="futbalmania")
+    
+    cursor = connection.cursor(dictionary=True)
+
+    statement = 'SELECT country_of_citizenship FROM players WHERE highest_market_value_in_eur >= 50000000 AND last_season > 2015 ORDER BY RAND() LIMIT 1;'
+   
+    cursor.execute(statement)
+    random_value = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    return random_value
---
+++ sd.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template, url_for,request
+from data_base import *
+import random
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+@app.route("/player")
+def player():
+    player_values = player_t()
+    return render_template('player.html', title='Player', result=player_values)
+
+
+@app.route("/quiz_game")
+def quiz_game():
+    q = question_game()
+    global global_variable
+    global_variable = q[0]['country_of_citizenship']
+    n = q[-1]['players_name']
+
+    v1 = random_value()
+    while v1[0]['country_of_citizenship'] == global_variable:
+        v1 = random_value()
+
+    v2 = random_value()
+    while v2[0]['country_of_citizenship'] == global_variable or v2[0]['country_of_citizenship'] == v1[0]['country_of_citizenship']:
+        v2 = random_value()
+
+    values = [global_variable, v1[0]['country_of_citizenship'], v2[0]['country_of_citizenship']]
+    random.shuffle(values)
+    
+    return render_template('quiz_game.html', title='Quiz Game', q=n, v0=values[0], v1=values[1], v2=values[2])
+
+
+
+@app.route("/quiz_game_result", methods=['POST'])
+def submit():
+    user_answer = request.form['answer']
+    correct_answer = global_variable
+    is_correct = user_answer == correct_answer
+    return render_template('quiz_game2.html', is_correct=is_correct, correct_answer=correct_answer,user_answer=user_answer)
+
+
+
+@app.route("/competitions")
+def competitions():
+    list0f_competitions = ["England", "Italy", "Germany", "Turkey"]
+    random_competition = random.choice(list0f_competitions)
+    return render_template('competitions.html', title='Competitions', random_competition=random_competition)
+
+@app.route("/games")
+def games():
+    game_competitions = game_get_comp()
+    game_season = game_get_season()
+    game_rounds = game_get_round()
+    game_clubs = game_get_clubs()
+    game_games = game_get_games()
+    return render_template('games.html', 
+                            title='Games',
+                            game_competitions = game_competitions,
+                            game_season = game_season,
+                            game_rounds = game_rounds,
+                            game_clubs = game_clubs,
+                            game_games = game_games)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
